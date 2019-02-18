b1f112d5d3b753884c1e87037390f4efbade2040
neomat91 <rahulramesh1995@gmail.com>
2019-02-17 22:53:29 -0700

    initialize game board

--- chess.js
+++ chess.js
@@ -1,1 +1,25 @@
-console.log('working');
+// CHESS 
+
+console.log('Welcome to my chess game %C3%B9');
+
+// Output the base chess piece positions
+//  pieces -> 
+//	 - Pawns 
+//       - Rooks
+// 	 - Knights 
+//       - Bishops
+//       - Queen
+// 	 - King
+
+var Chess = require('./chess_git_supp').Chess;
+
+var chess = new Chess();
+
+console.log(chess.ascii());
+
+
+
+
+
+
+ 
