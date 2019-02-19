e741f08ac8fc9c92ff225d06a66ea26d4df6a841
neomat91 <rahulramesh1995@gmail.com>
2019-02-19 03:19:54 -0700

    implement minmax algorithm first try

--- chess.js
+++ chess.js
@@ -14,22 +14,103 @@ console.log('Welcome to my chess game \u270F');
 const Chess = require('./chess_git_supp').Chess;
 //import Chess from "./chess_git_supp.js";
 
-const chess = new Chess();
+var chess = new Chess();
 
 class bst 
 {
 	static __base__()
 	{
-		return console.log(chess.ascii());	
+		console.log(chess.ascii());
+
 	}
 
-	static random()
+	static hier(pawn)
 	{
-		var _moves = chess.moves();
-		var rand_move  = _moves[Math.floor(Math.random()*_moves.length)];
-		chess.move(rand_move);
-		return console.log(chess.ascii());
+		//wp 10		bp -10
+		//wr 30		br -30
+		//wb 30		bb -30
+		//we 50		be -50
+		//wk 900	bk -900
+		//wq 90		bq -90
+
+		
+		for(var i=0;i<pawn.length;i++)
+		{
+			var calculated=[];
+
+			if(chess.turn()=='w')
+			{
+				if(pawn[i] == /K/)
+				{
+					var value = 900;
+				}
+				else if(pawn[i] == /Q/)
+				{
+					var value = 90;
+				}
+				else if(pawn[i] == /B/)
+				{
+					var value = 30;
+				}
+				else if(pawn[i] == /R/)
+				{
+					var value = 50;
+				}
+				else if(pawn[i] == /N/)
+				{
+					var value = 30;
+				}
+				else
+				{
+					var value = 10;
+				}
+				calculated[i] = value;
+			}
+		}
+		var max = Math.max(calculated);
+		var index = calculated.indexOf(max);
+		var fmove = pawn[index];
+		return fmove;
 	}
+
+	static random(counter)
+	{
+//		console.log(counter);
+//		if(fen)
+//		{
+//			var chess = new Chess(fen);
+//		}
+		var _moves = chess.moves();
+		var _move = bst.hier(_moves);
+		console.log(_move);
+//		var rand_move  = _moves[Math.floor(Math.random()*_moves.length)];
+		chess.move(_move);	
+//		var position = chess.fen();
+		if(counter < 3)
+		{
+			counter  = counter + 1;
+			bst.random(counter);
+		}
+		else
+		{
+			console.log(chess.ascii());
+		}
+	}
+
+
+
+	static min()
+	{
+		
+	}
+
+	static max()
+	{
+
+	}
+
+
 }
 
-bst.random();
+bst.random(0);
+
